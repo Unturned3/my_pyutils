7dff68c7942d8f76dce7123e329383cfb6d618c7
Yusen Hu <richardyusen@gmail.com>
2024-12-05 00:18:32 -0500

    Add option to enable Retina rendering

--- jupyter.py
+++ jupyter.py
@@ -9,7 +9,10 @@ magic_commands = [
 
 magic_commands = [s.split(' ', 1) for s in magic_commands]
 
-def jupyter_init():
+def jupyter_init(retina_rendering=False):
     ipy = get_ipython()
     for (cmd, args) in magic_commands:
         ipy.run_line_magic(cmd, args)
+    if retina_rendering:
+        import matplotlib_inline
+        matplotlib_inline.backend_inline.set_matplotlib_formats('retina')
